63c934f74962ebfa2f61709d1be34bb43d5884a0
Aishwarya Chavan <chavanaishwarya993@gmail.com>
2026-07-31 22:04:20 +0530

    Create positive.js

---
+++ positive.js
@@ -0,0 +1,12 @@
+let num=10;
+if(num > 0)
+{
+    console.log("the number is positive");
+}else
+if(num<0)
+{
+    console.log("the number is negative");
+}else
+{
+    console.log("the number is zero");
+}
