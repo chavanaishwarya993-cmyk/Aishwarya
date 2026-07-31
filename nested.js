d76cb20cd5e7ffadbb167b328c89c853533515b0
Aishwarya Chavan <chavanaishwarya993@gmail.com>
2026-07-31 22:03:33 +0530

    Create nested.js

---
+++ nested.js
@@ -0,0 +1,21 @@
+let employee={
+
+    employee1:{
+    ename="nisha",
+    salary:30000
+    },
+
+    employee2:{
+        ename:"geeta",
+        salary:50000
+    
+    },
+
+    employee3:{
+        ename:"sonu",
+        salary:60000
+        
+    }
+
+}
+console.log(employee);
