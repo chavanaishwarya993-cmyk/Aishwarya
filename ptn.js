b1352835f96adacb1acb09d2de5cb00a7bfc5863
Aishwarya Chavan <chavanaishwarya993@gmail.com>
2026-07-31 22:07:33 +0530

    Create ptn.js

---
+++ ptn.js
@@ -0,0 +1,51 @@
+var ptr="";
+for(i=1;i<=4;i++)
+{
+    for(j=1;j<=1;j++)
+    {
+        ptr+="*"
+    }
+    ptr+="\n"
+}
+
+
+
+
+
+var ptr="";
+for(i=1;i<=4;i++)
+{
+    for(j=1;j<=i;j++)
+    {
+        ptr+="*"
+    }
+    ptr+="\n"
+}
+
+
+var ptr=""
+for(i=1;i<=4;i++)
+{
+    for(j=1;j<=i;j++)
+    {
+        ptr+="*"
+    }
+    ptr+="\n"
+}
+console.log(ptr);
+
+var ptr=""
+for(i=1;i<=4;i++)
+{
+    for(j=3;j>=i;j--)
+    {
+        ptr+=""
+    }
+    for(k=1;k<=i;k++)
+    {
+        ptr+="*"
+    }
+    ptr+="\n"
+
+}
+console.log(ptr);
