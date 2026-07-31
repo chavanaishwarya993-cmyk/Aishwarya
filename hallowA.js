7047e9bdde53b9da95668c576fb2d5e84f81c264
Aishwarya Chavan <chavanaishwarya993@gmail.com>
2026-07-31 21:59:27 +0530

    Create hallowA.js

---
+++ hallowA.js
@@ -0,0 +1,13 @@
+var ptr="";
+for(i=1;i<=7;i++)
+{
+    for(j=1;j<=7;j++)
+    {
+        if(i==1||j==7)
+            ptr+="*"
+        else
+            ptr+=" "
+    }
+    ptr+="\n"
+}
+console.log(ptr);
